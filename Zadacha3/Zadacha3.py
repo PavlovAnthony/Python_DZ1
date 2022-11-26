2e0a748bc774613baa45720e4924b7b0219afd47
Anton <pavlov@mail.ru>
2022-11-26 22:32:37 +0500

    Zadacha3

--- Zadacha3/Zadacha3.py
+++ Zadacha3/Zadacha3.py
@@ -3,7 +3,7 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
-def get_room_number():
+def get_coord():
     while True:
         try:
             a, b = [float(s) for s in input(
@@ -13,7 +13,7 @@ def get_room_number():
             print("Ошибочный ввод.")
 
 
-x, y = get_room_number()
+x, y = get_coord()
 if x < 0 and y < 0:
     print(f"X: {x}, Y: {y}, 3-я четверть")
 elif x > 0 and y < 0:
