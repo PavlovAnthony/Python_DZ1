8b5cb235419d39ba98f46f3b0ec9a285766e20f6
Anton <pavlov@mail.ru>
2022-11-26 22:29:32 +0500

    Zadacha3

---
+++ Zadacha3/Zadacha3.py
@@ -0,0 +1,30 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+def get_room_number():
+    while True:
+        try:
+            a, b = [float(s) for s in input(
+                'введите координаты точки X и Y через пробел: ').split()]
+            return a, b
+        except ValueError:
+            print("Ошибочный ввод.")
+
+
+x, y = get_room_number()
+if x < 0 and y < 0:
+    print(f"X: {x}, Y: {y}, 3-я четверть")
+elif x > 0 and y < 0:
+    print(f"X: {x}, Y: {y}, 4-я четверть")
+elif x > 0 and y > 0:
+    print(f"X: {x}, Y: {y}, 1-я четверть")
+elif x < 0 and y > 0:
+    print(f"X: {x}, Y: {y}, 2-я четверть")
+elif x == 0 and y != 0:
+    print(f"X: {x}, Y: {y}, ось Y")
+elif y == 0 and x != 0:
+    print(f"X: {x}, Y: {y}, ось X")
+elif x == 0 and y == 0:
+    print(f"X: {x}, Y: {y}, Центр")
